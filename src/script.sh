13edf8afcc74a68153160e10b8c428d1f41a1a6c
spaul91 <sophie.paul@addenbrookes.nhs.uk>
2024-10-29 15:17:58 +0000

    script written

--- src/script.sh
+++ src/script.sh
@@ -5,13 +5,24 @@ set -e -x -o pipefail
 #   dx-jobutil-report-error "My error message"
 
 main() {
-
-    ## Download all inputs
     dx-download-all-inputs
-
-    ## load image
-    docker load /usr/src/ici_uploader_0.0.1.tar.gz
-
-    ## launch upload
-    docker run eastgenomics/ici_uploader:v0.0.1 --workflowId $workflow_id --folder $run_folder
+    # DNANexus apps don't accept folders as valid arguments, so we can't
+    # use dx-download-all-inputs. We need to use recursive dx download instead
+    dx download -r ${DX_PROJECT_CONTEXT_ID}:${run_folder} -o in/
+    export RUN_FOLDER_NAME=$(basename $run_folder)
+    # Start container process in background, and use docker exec to run multiple commands.
+    # This is because we need the docker container to persist between steps
+    sudo docker load -i /image/ici_uploader_v1.0.0.tar.gz
+    docker run \
+        --name uploader \
+        -e API_KEY_FILENAME=$api_key_file_name \
+        -e RUN_FOLDER=$run_folder \
+        -e WORKFLOW_ID=$workflow_id \
+        --mount type=bind,source=/home/dnanexus/in/,target=/in \
+        --entrypoint /bin/bash \
+        -itd eastgenomics/ici_uploader:v1.0.0
+    # bash -c with single quotes is the only way to use docker container's internal env variables
+    # two steps needed - authenticate, then upload
+    docker exec uploader bash -c './ici-uploader configure --api-key $(cat /in/api_key_file/${API_KEY_FILENAME})'
+    docker exec uploader bash -c './ici-uploader --workflowId $WORKFLOW_ID --folder /in/${RUN_FOLDER}'
 }
