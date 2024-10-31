7c9af6431ddea2b2b4972be37a05d34cde3801c2
spaul91 <sophie.paul@addenbrookes.nhs.uk>
2024-10-31 12:13:00 +0000

    script split into functions

--- src/script.sh
+++ src/script.sh
@@ -4,25 +4,83 @@ set -e -x -o pipefail
 # To report errors specifically:
 #   dx-jobutil-report-error "My error message"
 
+get_match_line_number(){
+
+    FILE=$1
+    PATTERN=$2
+
+    local LINE_NUMBER=$(grep -Enow "$PATTERN" $FILE | sed "s/:${PATTERN}//")
+    echo $LINE_NUMBER
+}
+
+extract_samples(){
+
+    SAMPLESHEET=$1
+    PATTERN="Sample_ID"
+
+    local LINE_NUMBER=$(get_match_line_number $SAMPLESHEET $PATTERN)
+    local START_AT=$(bc <<< "$LINE_NUMBER + 1")
+    tail -n+${START_AT} $SAMPLESHEET | cut -f1 -d,
+}
+
+make_case_metadata(){
+
+    SAMPLESHEET=$1
+
+    local SAMPLE_ARRAY=($(extract_samples $SAMPLESHEET))
+
+    echo "Sample_ID,Tumor_Type" > custom_case_metadata.csv
+
+    # the numeric string is the SNOMED ID for "Tumor of Unknown Origin"
+    for SAMPLE in ${SAMPLE_ARRAY[@]}; do
+        echo "$SAMPLE,255051004" >> custom_case_metadata.csv
+    done
+}
+
+handle_samplesheet() {
+    local SAMPLESHEET_PATH=$1
+    local SAMPLESHEET_DIRNAME=$(dirname $SAMPLESHEET_PATH)
+    local SAMPLESHEET_BASENAME=$(basename $SAMPLESHEET_PATH)
+    # here we coerce the samplesheet name to SampleSheet.csv
+    if ! egrep -q "^SampleSheet.csv" <<< $SAMPLESHEET_BASENAME; then
+        rename -d "s/.*SampleSheet.*.csv/SampleSheet.csv/" $SAMPLESHEET_PATH
+    fi
+    # no matter what we end up with SampleSheet.csv in our directory
+    # return it to be used by other functions
+    echo "$SAMPLESHEET_DIRNAME/SampleSheet.csv"
+}
+
 main() {
     dx-download-all-inputs
+
     # DNANexus apps don't accept folders as valid arguments, so we can't
     # use dx-download-all-inputs. We need to use recursive dx download instead
     dx download -r ${DX_PROJECT_CONTEXT_ID}:${run_folder} -o in/
-    export RUN_FOLDER_NAME=$(basename $run_folder)
+
+    # head -n1 returns the first match in the first directory where a samplesheet is found
+    ## Potential issue: what happens if there's more than one samplesheet in the topmost directory?
+    SAMPLESHEET_PATH=$(find in/ -name "*SampleSheet*.csv" | head -n1)
+    SAMPLESHEET_PATH_UPDATED=$(handle_samplesheet $SAMPLESHEET_PATH)
+    make_case_metadata $SAMPLESHEET_PATH_UPDATED
+    mv custom_case_metadata.csv in/
+
     # Start container process in background, and use docker exec to run multiple commands.
     # This is because we need the docker container to persist between steps
-    sudo docker load -i /image/ici_uploader_v1.0.0.tar.gz
+    DOCKER_IMAGENAME=$(find /image -name "*.tar.gz")
+    sudo docker load -i $DOCKER_IMAGENAME
+    DOCKER_IMAGE=$(docker image ls -q)
     docker run \
         --name uploader \
         -e API_KEY_FILENAME=$api_key_file_name \
-        -e RUN_FOLDER=$run_folder \
+        -e RUN_FOLDER_NAME=$(basename run_folder) \
         -e WORKFLOW_ID=$workflow_id \
         --mount type=bind,source=/home/dnanexus/in/,target=/in \
         --entrypoint /bin/bash \
-        -itd eastgenomics/ici_uploader:v1.0.0
+        -itd $DOCKER_IMAGE
+
     # bash -c with single quotes is the only way to use docker container's internal env variables
-    # two steps needed - authenticate, then upload
+    # three steps needed - authenticate, upload run, upload case metadata
     docker exec uploader bash -c './ici-uploader configure --api-key $(cat /in/api_key_file/${API_KEY_FILENAME})'
-    docker exec uploader bash -c './ici-uploader analysis upload --workflowId $WORKFLOW_ID --folder /in/${RUN_FOLDER}'
+    docker exec uploader bash -c './ici-uploader analysis upload --workflowId $WORKFLOW_ID --folder /in/${RUN_FOLDER_NAME}'
+    docker exec uploader bash -c './ici-uploader case-data --filePath /in/custom_case_metadata.csv'
 }
