da5cd3abb9ab4869be7b0f878235b9298350717a
spaul91 <sophie.paul@addenbrookes.nhs.uk>
2024-11-14 15:50:52 +0000

    updated for compliance with ShellCheck

--- src/script.sh
+++ src/script.sh
@@ -5,55 +5,80 @@ set -e -x -o pipefail
 #   dx-jobutil-report-error "My error message"
 
 download_run(){
-    RUN_PATH=$1
-    OUT_FOLDER=$(sed "s:/$::g" <<< $2) # removes trailing "/" if present
 
-    if grep -Eq "^project-" <<< $RUN_PATH; then
-        dx download -r $RUN_PATH -o $OUT_FOLDER
+    local RUN_PATH
+    RUN_PATH=$1
+
+    local OUT_FOLDER
+    OUT_FOLDER=${2/%\//} # removes trailing "/" if present
+
+    if grep -Eq "^project-" <<< "${RUN_PATH}"; then
+        dx download -r "${RUN_PATH}" -o "${OUT_FOLDER}"
     else
-        dx download -r ${DX_PROJECT_CONTEXT_ID}:${RUN_PATH} -o $OUT_FOLDER
+        dx download -r "${DX_PROJECT_CONTEXT_ID}":"${RUN_PATH}" -o "${OUT_FOLDER}"
     fi
 }
 
 get_match_line_number(){
 
+    local FILE
     FILE=$1
+
+    local PATTERN
     PATTERN=$2
 
-    local LINE_NUMBER=$(grep -Enow "$PATTERN" $FILE | sed "s/:${PATTERN}//")
-    echo $LINE_NUMBER
+    local LINE_NUMBER
+    LINE_NUMBER=$(grep -Enow "${PATTERN}" "${FILE}" | sed "s/:${PATTERN}//")
+
+    echo "${LINE_NUMBER}"
 }
 
 extract_samples(){
 
+    local SAMPLESHEET
     SAMPLESHEET=$1
+
+    local PATTERN
     PATTERN="Sample_ID"
 
-    local LINE_NUMBER=$(get_match_line_number $SAMPLESHEET $PATTERN)
-    local START_AT=$(bc <<< "$LINE_NUMBER + 1")
-    tail -n+${START_AT} $SAMPLESHEET | cut -f1 -d,
+    local LINE_NUMBER
+    LINE_NUMBER=$(get_match_line_number "${SAMPLESHEET}" "${PATTERN}")
+
+    local START_AT
+    START_AT=$(bc <<< "${LINE_NUMBER} + 1")
+
+    tail -n+"${START_AT}" "${SAMPLESHEET}" | cut -f1 -d,
 }
 
 make_case_metadata(){
 
+    local SAMPLESHEET
     SAMPLESHEET=$1
 
-    local SAMPLE_ARRAY=($(extract_samples $SAMPLESHEET))
+    local SAMPLE_ARRAY
+    mapfile SAMPLE_ARRAY <<< "$(extract_samples "${SAMPLESHEET}")"
 
     echo "Sample_ID,Case_ID,Tumor_Type" > custom_case_metadata.csv
 
-    # the numeric string is the SNOMED ID for "Tumor of Unknown Origin"
-    for SAMPLE in ${SAMPLE_ARRAY[@]}; do
+    # "255051004" is the SNOMED ID for "Tumor of Unknown Origin"
+    for SAMPLE in "${SAMPLE_ARRAY[@]}"; do
         echo "${SAMPLE},${SAMPLE},255051004" >> custom_case_metadata.csv
     done
 }
 
 handle_samplesheet() {
-    local SAMPLESHEET_PATH=$1
-    local SAMPLESHEET_DIRNAME=$(dirname $SAMPLESHEET_PATH)
-    local SAMPLESHEET_BASENAME=$(basename $SAMPLESHEET_PATH)
-    mv ${SAMPLESHEET_DIRNAME}/${SAMPLESHEET_BASENAME} ${SAMPLESHEET_DIRNAME}/SampleSheet.csv
-    echo "$SAMPLESHEET_DIRNAME/SampleSheet.csv"
+
+    local SAMPLESHEET_PATH
+    SAMPLESHEET_PATH=$1
+
+    local SAMPLESHEET_DIRNAME
+    SAMPLESHEET_DIRNAME=$(dirname "${SAMPLESHEET_PATH}")
+
+    local SAMPLESHEET_BASENAME
+    SAMPLESHEET_BASENAME=$(basename "${SAMPLESHEET_PATH}")
+
+    mv "${SAMPLESHEET_DIRNAME}/${SAMPLESHEET_BASENAME}" "${SAMPLESHEET_DIRNAME}/SampleSheet.csv"
+    echo "${SAMPLESHEET_DIRNAME}/SampleSheet.csv"
 }
 
 main() {
@@ -63,30 +88,30 @@ main() {
 
     # DNANexus apps don't accept folders as valid arguments, so we can't
     # use dx-download-all-inputs for the run folder. We need to use recursive dx download instead
-    download_run $run_folder in/
+    download_run "${run_folder}" in/
 
     # head -n1 returns the first match in the first directory where a samplesheet is found
     ## Potential issue: what happens if there's more than one samplesheet in the topmost directory?
     SAMPLESHEET_PATH=$(find in/ -iname "*SampleSheet*.csv" | head -n1)
-    SAMPLESHEET_PATH_UPDATED=$(handle_samplesheet $SAMPLESHEET_PATH)
-    make_case_metadata $SAMPLESHEET_PATH_UPDATED
+    SAMPLESHEET_PATH_UPDATED=$(handle_samplesheet "${SAMPLESHEET_PATH}")
+    make_case_metadata "${SAMPLESHEET_PATH_UPDATED}"
     mv custom_case_metadata.csv in/
 
     # Start container process in background, and use docker exec to run multiple commands.
     # This is because we need the docker container to persist between steps
     DOCKER_IMAGENAME=$(find /image -name "*.tar.gz")
-    sudo docker load -i $DOCKER_IMAGENAME
+    sudo docker load -i "${DOCKER_IMAGENAME}"
     DOCKER_IMAGE=$(docker image ls -q)
     docker run \
         --name uploader \
-        -e API_KEY_FILENAME=$api_key_file_name \
-        -e RUN_FOLDER_NAME=$(basename $run_folder) \
-        -e WORKFLOW_ID=$workflow_id \
+        -e API_KEY_FILENAME="${api_key_file_name}" \
+        -e RUN_FOLDER_NAME="$(basename "${run_folder}")" \
+        -e WORKFLOW_ID="${workflow_id}" \
         --mount type=bind,source=/home/dnanexus/in/,target=/in \
         --entrypoint /bin/bash \
-        -itd $DOCKER_IMAGE
+        -itd "${DOCKER_IMAGE}"
 
-    if [ $dry_run == true ]; then
+    if [ "${dry_run}" == true ]; then
         echo -e "#-----------\n\tPRINTING CUSTOM CASE DATA\n#-----------"
         docker exec uploader bash -c 'cat /in/custom_case_metadata.csv'
         echo -e "\n#-----------\n\tCONTENTS OF /in\n#-----------"
